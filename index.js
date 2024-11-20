5974447a329ed22baab793ef28ced9229d745e80
abdur rehman abid <156325681+Abdurrehman211@users.noreply.github.com>
2024-11-20 10:20:16 -0800

    Possible changes and Donation page is uploaded

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function Navigate(){
     window.location='Login.html';
 }
+function NavigatetoShop(){
+    window.location='Shop.html';
+}
+// change the donation to the screen where it is going to donate
+function Navigate1(){
+    window.location.href='#donation'
+}
